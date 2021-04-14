faea03ccb72fced94634c91ff5e4e85240046c1a
Denibilly <denibilly@hotmail.com>
2021-04-14 21:45:03 +0100

    heroku middleware

--- web-server.js
+++ web-server.js
@@ -2,13 +2,16 @@
 const express = require("express");
 const { join } = require("path");
 const morgan = require("morgan");
+var history = require('connect-history-api-fallback');
+
 const app = express();
 
 var port = process.env.PORT || 5001;
 
 app.use(morgan("dev"));
-
+app.use(history());
 app.use(express.static(join(__dirname, "dist")));
+
 app.use((_, res) => {
   res.sendFile(join(__dirname, "dist", "index.html"));
 });
