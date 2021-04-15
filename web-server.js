70817a0e5dad61822b9039b188779b38077ccca0
Denibilly <denibilly@hotmail.com>
2021-04-15 16:08:10 +0100

    update server heroku

--- web-server.js
+++ web-server.js
@@ -1,19 +1,33 @@
-/* eslint-disable no-console */
-const express = require("express");
-const { join } = require("path");
-const morgan = require("morgan");
-var history = require('connect-history-api-fallback');
 
-const app = express();
+
+app.listen(port, () => console.log("Listening on port "+port));
+
+var path = require('path');
+var express = require("express");
+const morgan = require("morgan");
+var app = express();
 
 var port = process.env.PORT || 5001;
 
 app.use(morgan("dev"));
-app.use(history());
+app.use(express.logger());
+app.use(express.compress());
+app.use(express.bodyParser());
 app.use(express.static(join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, '../dist'), { maxAge: 86400000 }));
 
-app.use((_, res) => {
-  res.sendFile(join(__dirname, "dist", "index.html"));
+//handle any errors
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.send(500, 'Something broke!');
 });
 
-app.listen(port, () => console.log("Listening on port "+port));
+// Render the app
+app.get('/', function(req, res) {
+  res.sendfile(path.join(__dirname, '../dist/index.html'));
+});
+
+//start server
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
